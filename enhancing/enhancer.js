730c1b7a277bf2fac738e46c4ac16e3f193329af
CWC-88 <29718763+CWC-88@users.noreply.github.com>
2020-07-21 15:37:38 -0500

    asdfasfa

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,33 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if(20>=item.enhancemnt){
+    return{...item,enhancemnt:item.enhancemnt+1}
+  }else{
+    return { ...item };
+  }
+
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancemnt<15){
+  return { ...item,durability:durability-5 };
+  }
+  // If the item's enhancement is 15 or more,
+  //  the durability of the item is decreased by 10.
+
+  // If the item's enhancement level is greater than 16, 
+  // the enhancement level decreases by 1
+
+  else if(item.enhancemnt>=15){
+    return { ...item,durability:durability-10 };
+  }
+  else if(item.enhancemnt>16)
+  return{...item, enhancement:item.enhancemnt-1}
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,38 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('repair',()=>{
+  it('resets hp to 100',()=>{
+      const item = {
+          name:"hundo",
+          durability:88,
+          enhancement:7
+      }
+      expect(enhancer.repair(item)).toBe(100)
+  })  
+})
+
+//successful repair
+describe('successful repair',()=>{
+    it('increase hp to 1'),()=>{
+        let item={
+            name:"hundo",
+            durability:88,
+            enhancement:15  
+        }
+        expect(enhancer.succeed(item)).toBe(16)
+    }
+
+})
+
+describe('fail',()=>{
+    it('decrement by 5 if >15 ',()=>{
+
+        let item={
+            name:"hundo",
+            durability:88,
+            enhancement:15  
+        }
+        expect(enhancer.fail(item)).toBe(9)
+    })
+})
+
